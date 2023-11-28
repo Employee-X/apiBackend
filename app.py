d328bb5d32ccd57251d0e4f380d6767ffe6edbe0
TOXIC <shm27062003@gmail.com>
2023-11-28 21:14:51 +0530

    ....

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-from fastapi import FastAPI,Request
+from fastapi import FastAPI
+
 from config.config import initiate_database
+from fastapi.middleware.cors import CORSMiddleware
 
 from api.routes.user import router as UserRouter
 from api.routes.jobSeeker import router as JSRouter
@@ -13,6 +15,22 @@ recruiter_collection = DbUserModels.Recruiter
 
 app = FastAPI()
 
+origins = [
+    "http://localhost:3000",
+    "http://localhost:3001",
+    "http://www.employeex.co.in",
+    "https://www.employeex.co.in",
+    "http://frontend-test-env.eba-qubingfp.ap-south-1.elasticbeanstalk.com",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 @app.on_event("startup")
 async def start_database():
     await initiate_database()
@@ -24,10 +42,8 @@ async def start_database():
  
 
 @app.get("/", tags=["Root"])
-async def read_root(request: Request):
-    return {"message": "Hello World",
-            "client": request.client.host
-            }
+async def read_root():
+    return {"message": "Hello World"}
 
 @app.get("/getOpenings",tags=["Root"])
 async def get_openings():
